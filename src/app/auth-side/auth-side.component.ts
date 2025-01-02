dd34e19355df86e226779d2043e34a1442c3ace6
Sahil Khimani <s.mapmf4u@gmail.com>
2025-01-02 16:42:24 +0500

    login page

--- src/app/auth-side/auth-side.component.ts
+++ src/app/auth-side/auth-side.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-side',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './auth-side.component.css'
 })
 export class AuthSideComponent {
-
+  @Input() backgroundImage : string = '';
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
 import { AuthSideComponent } from '../auth-side/auth-side.component';
-import { RouterLink } from '@angular/router';
+import { RouteConfigLoadEnd, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [AuthSideComponent, RouterLink],
+  imports: [
+    AuthSideComponent,
+    RouterLink,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  
+  backgroundImage = 'assets/images/loginside.jpg'
+  showPassword = false;
 
+  loginForm = new FormGroup({
+    email : new FormControl('',
+     [Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]),
+    password : new FormControl('',
+      [Validators.required, 
+      Validators.minLength(8),
+      Validators.pattern('^(?=.*[A-Z]).{8,}$')]) 
+  })
+  
+    showPass(){
+      this.showPassword = !this.showPassword;
+    }
+
+  submitForm(){
+    
+  }
+
+  get f(){
+    return this.loginForm.controls;
+  }
+
+  getErrorMessages(controlName : string){
+    const control = this.loginForm.get(controlName);
+    if(control?.hasError('required')){
+      return 'This field is required';
+    }
+    if(controlName == 'email' && control?.hasError('pattern')){
+      return 'Enter valid Email Address (e.g. user@example.com)'
+    }
+    if(controlName == 'password' && control?.hasError('pattern')){
+      return 'Pasword must contain atleast 8 character with one uppercase letter'
+    }
+    return '';
+  }
 }
