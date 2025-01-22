87026a38f6a9b66835832c2beaa0047b4d99f510
Sahil Khimani <s.mapmf4u@gmail.com>
2025-01-22 19:25:44 +0500

    [fix] login component

--- src/app/auth-pages/login/login.component.ts
+++ src/app/auth-pages/login/login.component.ts
@@ -5,10 +5,6 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { CommonModule } from '@angular/common';
 import { LoginModel } from '../../models/login.model';
 import { UserService } from '../../services/user.service';
-import { LocalStorageService } from '../../utility/services/local-storage.service';
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
-import { MatButton } from '@angular/material/button';
-import { config } from 'rxjs';
 import { SnackbarService } from '../../utility/services/snackbar.service';
 import { AppComponent } from '../../app.component';
 import { DecodeTokenService } from '../../utility/services/decode-token.service';
